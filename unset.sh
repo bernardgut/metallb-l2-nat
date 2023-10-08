fbf3700054fd5377c5a95875453f78727cfbce96
Bernard Gutermann <bernard.gutermann@gmail.com>
2023-10-09 00:04:36 +0200

    fix typo

--- unset.sh
+++ unset.sh
@@ -9,4 +9,4 @@ MACVLN=k8s.$1
 
 sudo ip link delete "$MACVLN"
 kubectl -n metallb-system delete ipaddresspools.metallb.io "$NAME"
-kubectl -n metallb-system delete l2advertisements.metallb.io "$NAME"
+kubectl -n metallb-system delete l2advertisements.metallb.io "$NAME"-bc
